3f5d7aea744892fee4298435facf1adcb556fa2e
Aswin P T <aswin.pt@experionglobal.com>
2024-11-23 14:53:26 +0530

    Vercel develop env setup

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -1,4 +1,4 @@
-//For development deploy
+//Environment for development deploy
 export const environment = {
     production: false,
     apiUrl: `https://online-assessment-tool-backend-dev.onrender.com`
